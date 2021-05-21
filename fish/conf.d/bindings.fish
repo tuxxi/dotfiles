e156192339da929ef23cb033879a111e7a4d23fc
Aidan Sojourner <aidan@sojourner.me>
2021-05-21 01:59:38 -0700

    [fish]: fix !! binding

--- fish/conf.d/bindings.fish
+++ fish/conf.d/bindings.fish
@@ -1,39 +1,19 @@
-# enables !!
+# stolen from https://github.com/fish-shell/fish-shell/issues/288#issuecomment-591679913
+
 function bind_bang
-  switch (commandline -t)
-  case "!"
-    commandline -t $history[1]; commandline -f repaint
-  case "*"
-    commandline -i !
-  end
+    switch (commandline --current-token)[-1]
+    case "!"
+        # Without the `--`, the functionality can break when completing
+        # flags used in the history (since, in certain edge cases
+        # `commandline` will assume that *it* should try to interpret
+        # the flag)
+        commandline --current-token -- $history[1]
+        commandline --function repaint
+    case "*"
+        commandline --insert !
+    end
 end
 
-# enables !$
-function bind_dollar
-  switch (commandline -t)
-  case "!"
-    commandline -t ""
-    commandline -f history-token-search-backward
-  case "*"
-    commandline -i '$'
-  end
-end
-
-# enables $?
-function bind_status
-  commandline -i (echo '$status')
-end
-
-# enables $$
-function bind_self
-  commandline -i (echo '$fish_pid')
-end
-
-# enable keybindings
 function fish_user_key_bindings
-  bind '$' bind_bang
-  bind '$' bind_dollar
-  bind '$?' bind_status
-  bind '$$' bind_self
+    bind ! bind_bang
 end
-
